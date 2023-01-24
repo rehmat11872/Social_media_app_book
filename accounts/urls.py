cbaa6d49169faab10838e96a30ccedcdc1af3e6d
RehmatQadeer <82605080+rehmat11872@users.noreply.github.com>
2023-01-24 15:08:25 +0500

    logout functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ from django.contrib.auth.views import LoginView
 
 urlpatterns = [
      path('signup/', views.SignUp.as_view(), name='register'),
-     path('login/', LoginView.as_view(), name='login')
+     path('login/', LoginView.as_view(), name='login'),
+     path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
