ab2d284a6c6351bbc9df282da18c0149820c1235
RehmatQadeer <82605080+rehmat11872@users.noreply.github.com>
2023-01-26 12:51:25 +0500

    Post Feed Updated Section

--- core/views.py
+++ core/views.py
@@ -16,10 +16,24 @@ class IndexView(LoginRequiredMixin, TemplateView):
    def get(self, request, *args, **kwargs):
         user_object = User.objects.get(username=request.user.username)
         user_profile = Profile.objects.get(user=user_object)
-        posts = Post.objects.all()
+
+
+        user_following_list = []
+        feed = []
+
+        user_following = FollowersCount.objects.filter(follower=request.user.username)
+
+        for users in user_following:
+            user_following_list.append(users.user)
+
+        for usernames in user_following_list:
+            feed_lists = Post.objects.filter(user=usernames)
+            feed.append(feed_lists)
+
+        feed_list = list(chain(*feed))
         context = {
             'user_profile':user_profile,
-            'posts': posts
+            'posts': feed_list
         }
         return render(self.request, self.template_name, context=context)
 
